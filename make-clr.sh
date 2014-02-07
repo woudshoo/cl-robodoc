fbc4f03b23bc0d8d3cf0a6530863f485c0018232
Wim Oudshoorn <Wim.Oudshoorn@intellimagic.net>
2014-02-07 16:52:39 +0100

    Fixed problem with resource directory, updated buildapp

--- make-clr.sh
+++ make-clr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-buildapp --output clr --load "/Users/woudshoo/quicklisp/setup"  --load-system "cl-robodoc" --eval "(cl-robodoc::initialize-resources)" --entry cl-robodoc::main
+buildapp --output clr --load "~/quicklisp/setup"  --load-system "cl-robodoc" --eval "(cl-robodoc::initialize-resources)" --entry cl-robodoc::main
